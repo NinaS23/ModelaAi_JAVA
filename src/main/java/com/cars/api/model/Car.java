e7beb260073300795d79f87a3bd5cde392d031e6
DUDA <mariabevenuto062@gmail.com>
2023-04-03 12:18:31 -0300

    feat: creat columns

--- src/main/java/com/cars/api/model/Car.java
+++ src/main/java/com/cars/api/model/Car.java
@@ -1,5 +1,6 @@
 package com.cars.api.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Data;
 
@@ -7,9 +8,19 @@ import lombok.Data;
 @Entity
 public class Car {
     private Long id;
+    
+    @Column(length = 15, nullable = false)
     private String modelo;
+
+    @Column(length = 20, nullable = false)
     private String fabricante;
+
+    @Column(length = 10, nullable = false)
     private String dataFabricacao;
+
+    @Column(nullable = false)
     private Double valor;
+
+    @Column(nullable = false)
     private int anoModelo;
 }
