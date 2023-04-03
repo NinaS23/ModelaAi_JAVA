d1b2a64abcac00ffacced9ba5f3b80e640c6d249
DUDA <mariabevenuto062@gmail.com>
2023-04-03 12:37:50 -0300

    feat: repository

--- src/main/java/com/cars/api/model/Car.java
+++ src/main/java/com/cars/api/model/Car.java
@@ -1,12 +1,30 @@
 package com.cars.api.model;
 
+import com.cars.api.dto.CarsDTO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class Car {
+
+    public Car(CarsDTO req) {
+		this.modelo = req.modelo();
+		this.fabricante = req.fabricante();
+		this.anoModelo = req.anoModelo();
+		this.valor = req.valor();
+		this.dataFabricacao = req.dataFabricacao();
+	}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
     @Column(length = 15, nullable = false)
